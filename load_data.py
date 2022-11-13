33e0f0a569acdee9247dd4fcf9c7d0a3fb7ace29
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-11-13 11:19:15 -0800

    Add todo file and load_data.py other methods

--- load_data.py
+++ load_data.py
@@ -8,4 +8,19 @@ def get_data():
     df2021 = pd.read_json('data/nvdcve-1.1-2021.json')
     frames = [df2018, df2019, df2020, df2021]
     return pd.concat(frames)
-    # return df2018
+
+
+def get_2018_data():
+    return pd.read_json('data/nvdcve-1.1-2018.json')
+
+
+def get_2019_data():
+    return pd.read_json('data/nvdcve-1.1-2019.json')
+
+
+def get_2020_data():
+    return pd.read_json('data/nvdcve-1.1-2020.json')
+
+
+def get_2021_data():
+    return pd.read_json('data/nvdcve-1.1-2021.json')
